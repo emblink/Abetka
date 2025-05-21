0f8919945378d5c4bc08df37fa6e210708478ce4
OleksiiTeplyi <oleksiy.tepliy@developex.net>
2025-05-21 01:07:29 -0400

    added sdcard + usb mass storage library, moved lvgl to lib folder, move src files to src folder, add dfplayer sounds

---
+++ src/Abetka.cpp
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/spi.h"
+#include "hardware/dma.h"
+#include "hardware/uart.h"
+#include "ws2812.h"
+#include "dfplayer.h"
+#include "lvgl.h"
+#include "st7789.h"
+#include "appMode.h"
+#include "mifareHal.h"
+#include "keyScan.h"
+#include "battery.h"
+#include <tusb.h>
+#include "tusb_config.h"
+
+// How to build without optimizations
+// cmake -DCMAKE_BUILD_TYPE=Debug -DPICO_DEOPTIMIZED_DEBUG=1 ..
+// ninja
+/**
+ * @brief Pin definitions for the DFPlayer.
+ * Another pair of TX/RX pins can be used, just
+ * be sure to address the relative uart instance.
+ */
+#define GPIO_TX         8       // To RX on the player
+#define GPIO_RX         9       // To TX on the player
+#define DFPLAYER_UART   uart1
+#define ST7789_SPI      spi1
+
+#define BATTERY_CHECK_PERIOD_MS (60 * 1000)
+
+dfplayer_t dfplayer = {0};
+
+// lcd configuration
+static const struct st7789_config lcd_config = {
+    .spi      = ST7789_SPI,
+    .gpio_din = 11,
+    .gpio_clk = 10,
+    .gpio_cs  = -1,
+    .gpio_dc  = 13,
+    .gpio_rst = 14,
+    .gpio_bl  = 15,
+};
+static const int lcd_width = 240;
+static const int lcd_height = 240;
+static uint16_t frameBuff[lcd_width * lcd_height] = {0};
+static lv_display_t * display = NULL;
+static uint32_t lastBatteryCheckMs = 0;
+static int32_t batteryPercent = 0;
+
+static bool isBntPressed(uint gpio)
+{
+    bool isPressed = false == gpio_get(gpio);
+    return isPressed;
+}
+
+void display_flush_cb(lv_display_t * display, const lv_area_t * area, uint8_t * px_map)
+{
+    /* The most simple case (also the slowest) to send all rendered pixels to the
+     * screen one-by-one.  `put_px` is just an example.  It needs to be implemented by you. */
+    uint16_t * buf16 = (uint16_t *)px_map; /* Let's say it's a 16 bit (RGB565) display */
+    int32_t x, y;
+    st7789_set_cursor(0, 0);
+    for(y = area->y1; y <= area->y2; y++) {
+        for(x = area->x1; x <= area->x2; x++) {
+            st7789_put(*buf16);
+            buf16++;
+        }
+    }
+    // TODO: O.T add fast write for the whole frame 
+
+    /* IMPORTANT!!!
+     * Inform LVGL that flushing is complete so buffer can be modified again. */
+    lv_display_flush_ready(display);
+}
+
+// Millisecond timestamp — you need this for `lv_tick_inc`
+static uint32_t getTimeMs(void) {
+    return to_ms_since_boot(get_absolute_time());
+}
+
+void batteryProcess()
+{
+    if (getTimeMs() - lastBatteryCheckMs < BATTERY_CHECK_PERIOD_MS) {
+        return;
+    }
+
+    batteryPercent = batteryGetPercent();
+    if (batteryPercent <= 0) {
+        appModeSwitch(APP_MODE_DISCHARGED);
+    }
+}
+
+int main()
+{
+    stdio_init_all();
+
+    // TODO: O.T figure out how to utilize lvgl st7789 driver instead of custom one.
+    st7789_init(&lcd_config, lcd_width, lcd_height);
+    st7789_fill(0xFFFF);
+    lv_init();
+    display = lv_display_create(lcd_width, lcd_height);
+    lv_display_set_buffers(display, (void *) frameBuff, NULL, sizeof(frameBuff), LV_DISPLAY_RENDER_MODE_FULL);
+    lv_display_set_flush_cb(display, display_flush_cb);
+    lv_tick_set_cb(getTimeMs);
+
+    mifareHalInit();
+
+    ws2812Init();
+
+    dfplayer_init(&dfplayer, DFPLAYER_UART, GPIO_TX, GPIO_RX);
+    sleep_ms(10);
+    dfplayer_set_volume(&dfplayer, 30);
+    sleep_ms(10);
+    dfplayer_set_eq(&dfplayer, EQ_BASS);
+    sleep_ms(10);
+    dfplayer_set_playback_mode(&dfplayer, MODE_FOLDER_REPEAT);
+    sleep_ms(200);
+    // dfplayer_play(&dfplayer, 2);
+
+    batteryInit();
+    batteryPercent = batteryGetPercent();
+    if (batteryPercent <= 0) {
+        appModeSwitch(APP_MODE_DISCHARGED);
+    } else {
+        appModeSwitch(APP_MODE_IDLE);
+    }
+
+    tud_init(0);
+    // TODO: O.T add indication module
+
+    while(1)
+    {
+        keyScanProcess();
+        batteryProcess();
+        appModeProcess();
+        tud_task();
+        lv_timer_handler();
+        sleep_ms(40);
+    }
+}
---
+++ src/appModeIdle.c
@@ -0,0 +1,98 @@
+#include "appModeIdle.h"
+#include "appMode.h"
+#include "lvgl.h"
+#include "mifareHal.h"
+#include "keyScan.h"
+
+// #define NUM_LETTERS 6
+
+// static const char *abetka_letters[NUM_LETTERS] = {"А", "б", "е", "т", "к", "а"};
+// static lv_obj_t *letter_labels[NUM_LETTERS];
+
+// static void anim_fade_cb(void *obj, int32_t v) {
+//     lv_obj_set_style_opa((lv_obj_t *) obj, v, 0);  // Cast до lv_obj_t*
+// }
+
+// void create_abetka_animation(lv_obj_t *parent) {
+//     // Стиль для контейнера з чорним фоном
+//     lv_obj_t * canvas = lv_canvas_create(lv_screen_active());
+//     lv_canvas_set_buffer(canvas, canvas_buf, 180, 180, render_cf);
+
+//     lv_canvas_fill_bg(canvas, lv_palette_lighten(LV_PALETTE_LIGHT_BLUE, 2), LV_OPA_COVER);
+
+//     static lv_style_t style_bg;
+//     lv_style_init(&style_bg);
+//     lv_style_set_bg_color(&style_bg, LV_COLOR_BLUE);
+//     lv_style_set_bg_opa(&style_bg, LV_OPA_COVER);
+
+//     // Стиль для тексту з жовтим кольором
+//     static lv_style_t style_text;
+//     lv_style_init(&style_text);
+//     lv_style_set_text_color(&style_text, lv_color_hex(0xFFFF00));  // Жовтий
+//     lv_style_set_text_font(&style_text, &lv_font_ukrainian_48);
+
+//     // Контейнер
+//     lv_obj_t *cont = lv_obj_create(parent);
+//     lv_obj_set_size(cont, 240, 100);
+//     lv_obj_center(cont);
+//     lv_obj_add_style(cont, &style_bg, 0);  // Додаємо чорний фон
+//     lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_ROW);
+//     lv_obj_set_style_pad_gap(cont, 10, 0);
+
+//     for (int i = 0; i < NUM_LETTERS; i++) {
+//         lv_obj_t *label = lv_label_create(cont);
+//         lv_label_set_text(label, abetka_letters[i]);
+//         lv_obj_add_style(label, &style_text, 0); // Жовтий текст
+//         lv_obj_set_style_opa(label, LV_OPA_TRANSP, 0);
+//         letter_labels[i] = label;
+
+//         lv_anim_t a;
+//         lv_anim_init(&a);
+//         lv_anim_set_var(&a, label);
+//         lv_anim_set_values(&a, LV_OPA_TRANSP, LV_OPA_COVER);
+//         lv_anim_set_time(&a, 200);
+//         lv_anim_set_delay(&a, i * 200);
+//         lv_anim_set_exec_cb(&a, anim_fade_cb);
+//         lv_anim_start(&a);
+//     }
+// }
+
+void lv_example_hello(void)
+{
+    /*Change the active screen's background color*/
+    lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x003a57), LV_PART_MAIN);
+
+    /*Create a white label, set its text and align it to the center*/
+    lv_obj_t * label = lv_label_create(lv_screen_active());
+    lv_label_set_text(label, "Абетка\nРомана");
+    lv_obj_set_style_text_font(label, &lv_font_ukrainian_48, LV_PART_MAIN);
+    lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
+    // lv_obj_set_style_text_font(label, &lv_font_montserrat_48, LV_PART_MAIN);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+}
+
+static void processInput(Key key, KeyState event)
+{
+    if (KEY_LEFT == key && KEY_STATE_PRESSED == event) {
+        appModeSwitch(APP_MODE_READ_CARD);
+    }
+}
+
+void appModeIdleEnter()
+{
+    keyScanInit(processInput);
+    lv_disp_set_theme(NULL, lv_theme_default_init(NULL, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_YELLOW), false, LV_FONT_DEFAULT));
+    // lv_obj_t *scr = lv_scr_act(); // Поточний активний екран
+    // create_abetka_animation(scr); // Стартуємо анімацію
+    lv_example_hello();
+}
+
+void appModeIdleProcess()
+{
+    // TODO: O.T play animation
+}
+
+void appModeIdleExit()
+{
+    keyScanDeinit();
+}
